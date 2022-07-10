c9e05be8412b664637120e096d95996b58172784
Barath Raj <rajbarathk@gmail.com>
2022-07-10 14:58:02 +0530

    [Barath] Add. Controller integration test for creating todo

--- src/test/java/com/tw/todo/TodoControllerIntegrationTest.java
+++ src/test/java/com/tw/todo/TodoControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.tw.todo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,14 +9,15 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -41,12 +43,55 @@ public class TodoControllerIntegrationTest {
 
         List<Todo> todos = new ArrayList<>();
         todos.add(new Todo("Title 1", false));
-        todos.add(new Todo("Topic 2", false));
+        todos.add(new Todo("Title 2", false));
         todoRepository.saveAll(todos);
 
         mockMvc.perform(get("/todo"))
                 .andExpect(status().isOk())
-                .andExpect(content().json(new ObjectMapper().writeValueAsString(todos)));
+                .andExpect(content().json(
+                        toJson(todos)
+                ));
 
     }
+
+    @Test
+    void shouldBeAbleToGetATodo() throws Exception {
+
+        List<Todo> todos = new ArrayList<>();
+        todos.add(new Todo("Title 1", false));
+        todos.add(new Todo("Title 2", false));
+        todoRepository.saveAll(todos);
+
+        mockMvc.perform(get("/todo/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        toJson(todos.get(1))
+                ));
+    }
+
+    @Test
+    void shouldBeAbleToCreateATodo() throws Exception {
+
+        Todo todo = new Todo("Title 1", false);
+
+        mockMvc.perform(post("/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(todo)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value(todo.getTitle()))
+                .andExpect(jsonPath("$.completed").value(todo.isCompleted()));
+
+    }
+
+
+
+
+    private String toJson(Todo todo) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(todo);
+    }
+
+    private String toJson(List<Todo> todos) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(todos);
+    }
+
 }
